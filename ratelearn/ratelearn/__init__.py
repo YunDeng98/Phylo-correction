ed09b41ca2a3ef402a7300c1c2f3b37696a92cb5
Pierre Boyeau <pierre.boyeau@gmail.com>
2021-06-23 10:52:52 -0700

    ratelearn module

--- ratelearn/ratelearn/__init__.py
+++ ratelearn/ratelearn/__init__.py
@@ -1,6 +1,7 @@
 from .rate import RateMatrix
 from .simulate import convert_triplet_to_quantized, generate_data
 from .trainer import train_quantization, estimate_likelihood, train_quantization_N, train_diag_param
+from .ratelearner import RateMatrixLearner
 
 __version__ = "0.1.0"
 __all__ = [
@@ -11,4 +12,5 @@ __all__ = [
     "train_quantization_N",
     "estimate_likelihood",
     "train_diag_param",
+    "RateMatrixLearner",
 ]

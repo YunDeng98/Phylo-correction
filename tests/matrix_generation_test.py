3e58537f5c2f4ba89b0bef67e1df9956d5a42015
Sebastian Prillo <sprillo@otus.EECS.Berkeley.EDU>
2021-07-05 03:56:49 -0700

    Add tests/matrix_generation_test.py

---
+++ tests/matrix_generation_test.py
@@ -0,0 +1,36 @@
+import os
+import unittest
+import tempfile
+from filecmp import dircmp
+from parameterized import parameterized
+
+from src.matrix_generation import MatrixGenerator
+
+
+class TestMatrixGenerator(unittest.TestCase):
+    @parameterized.expand([("multiprocess", 3), ("single-process", 1)])
+    def test_basic_regression(self, name, n_process):
+        """
+        Test that MatrixGenerator runs and its output matches the expected output.
+        The expected output is located at test_input_data/matrices_small
+
+        We run the same MatrixGenerator twice: first without caching,
+        then with caching, to make sure that caching works.
+        """
+        with tempfile.TemporaryDirectory() as root_dir:
+            outdir = os.path.join(root_dir, 'maximum_parsimony')
+            for use_cached in [False, True]:
+                contact_generator = MatrixGenerator(
+                    a3m_dir='test_input_data/a3m_small',
+                    transitions_dir='test_input_data/transitions_small',
+                    n_process=n_process,
+                    expected_number_of_MSAs=3,
+                    outdir=outdir,
+                    max_families=3,
+                    num_sites=1,
+                    use_cached=use_cached,
+                )
+                contact_generator.run()
+                dcmp = dircmp(outdir, 'test_input_data/matrices_small')
+                diff_files = dcmp.diff_files
+                assert(len(diff_files) == 0)

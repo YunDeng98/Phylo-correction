78128f854f13149dd6b1803d10b64257107a07f5
Sebastian Prillo <sprillo@otus.EECS.Berkeley.EDU>
2021-10-04 18:28:37 -0700

    Update xrate test

--- tests/xrate_test.py
+++ tests/xrate_test.py
@@ -43,28 +43,30 @@ class TestXRATEInputGenerator(unittest.TestCase):
         output. The expected output is located at test_input_data/Q1_XRATE_small
         """
         install_xrate()
-        with tempfile.TemporaryDirectory() as root_dir:
-            outdir = os.path.join(root_dir, 'xrate')
-            for use_cached in [False, True]:
-                xrate = XRATE(
-                    a3m_dir_full='test_input_data/a3m_small',
-                    xrate_input_dir='test_input_data/stockholm_small',
-                    expected_number_of_MSAs=3,
-                    outdir=outdir,
-                    max_families=3,
-                    use_cached=use_cached,
-                )
-                xrate.run()
-                learned_matrix_true_path = "test_input_data/Q1_XRATE_small/learned_matrix.txt"
-                learned_matrix_inferred_path = os.path.join(outdir, "learned_matrix.txt")
-                Q_true = np.loadtxt(learned_matrix_true_path)
-                Q_inferred = np.loadtxt(learned_matrix_inferred_path)
-                l1_error = np.sum(np.abs(Q_true - Q_inferred))
-                assert(l1_error < 0.01)
+        for xrate_grammar in [None, 'test_input_data/xrate_grammars/nullprot.eg']:
+            with tempfile.TemporaryDirectory() as root_dir:
+                outdir = os.path.join(root_dir, 'xrate')
+                for use_cached in [False, True]:
+                    xrate = XRATE(
+                        a3m_dir_full='test_input_data/a3m_small',
+                        xrate_input_dir='test_input_data/stockholm_small',
+                        expected_number_of_MSAs=3,
+                        outdir=outdir,
+                        max_families=3,
+                        xrate_grammar=xrate_grammar,
+                        use_cached=use_cached,
+                    )
+                    xrate.run()
+                    learned_matrix_true_path = "test_input_data/Q1_XRATE_small/learned_matrix.txt"
+                    learned_matrix_inferred_path = os.path.join(outdir, "learned_matrix.txt")
+                    Q_true = np.loadtxt(learned_matrix_true_path)
+                    Q_inferred = np.loadtxt(learned_matrix_inferred_path)
+                    l1_error = np.sum(np.abs(Q_true - Q_inferred))
+                    assert(l1_error < 0.01)
 
-                learned_matrix_true_path = "test_input_data/Q1_XRATE_small/learned_matrix_normalized.txt"
-                learned_matrix_inferred_path = os.path.join(outdir, "learned_matrix_normalized.txt")
-                Q_true = np.loadtxt(learned_matrix_true_path)
-                Q_inferred = np.loadtxt(learned_matrix_inferred_path)
-                l1_error = np.sum(np.abs(Q_true - Q_inferred))
-                assert(l1_error < 0.01)
+                    learned_matrix_true_path = "test_input_data/Q1_XRATE_small/learned_matrix_normalized.txt"
+                    learned_matrix_inferred_path = os.path.join(outdir, "learned_matrix_normalized.txt")
+                    Q_true = np.loadtxt(learned_matrix_true_path)
+                    Q_inferred = np.loadtxt(learned_matrix_inferred_path)
+                    l1_error = np.sum(np.abs(Q_true - Q_inferred))
+                    assert(l1_error < 0.01)

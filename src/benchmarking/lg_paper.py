e3ce5d913836d7d9cdab45e67357bfa6cc5af4b5
Sebastian Prillo <sprillo@otus.EECS.Berkeley.EDU>
2021-11-10 21:53:57 -0800

    Refactor lg_paper.py to enable using arbitrary models, and add file of LG results

--- src/benchmarking/lg_paper.py
+++ src/benchmarking/lg_paper.py
@@ -12,12 +12,26 @@ import wget
 
 from src.benchmarking import caching
 from src.phyml.phyml import (
-    get_number_of_sites,
-    get_number_of_taxa,
-    get_phyml_ll,
+    get_phyml_ll_from_phyml_stats,
     run_phyml,
 )
 from src.utils import pushd, verify_integrity_of_directory
+
+
+MODELS = [
+    ("JTT", "JTT", None),
+    ("WAG", "WAG", None),
+    # ("WAG_PAML", None, "./input_data/synthetic_rate_matrices/PAML/WAG.PAML.txt"),
+    ("r__LG", "r__LG", None),
+    ("LG", "LG", None),
+    ("Cherry_LG", None, "./input_data/synthetic_rate_matrices/PAML/Q_learnt_from_LG.PAML.txt"),
+    ("Cherry_LG_nosr", None, "./input_data/synthetic_rate_matrices/PAML/Q_learnt_from_LG_no_site_rates.PAML.txt"),
+    ("r__WAG'", "r__WAG'", None),
+    # ("LG_PAML", None, "./input_data/synthetic_rate_matrices/PAML/lg_LG.PAML.txt"),
+    # ("Cherry_LG_nosr", None, "./input_data/synthetic_rate_matrices/PAML/Q_learnt_from_LG_no_site_rates.PAML.txt"),
+    # ("Cherry_PFAM_32", None, "./input_data/synthetic_rate_matrices/PAML/Q_learnt_from_PFAM_32.PAML.txt"),
+    # ("Cherry_PFAM_32_nosr", None, "./input_data/synthetic_rate_matrices/PAML/Q_learnt_from_PFAM_32_no_site_rates.PAML.txt"),
+]
 
 
 def init_logger():
@@ -208,6 +222,48 @@ def run_phyml_cached(
         return phyml_stats, phyml_site_ll
 
 
+def get_phyml_ll(
+    input_msa_path: str,
+    model: Optional[str],
+    rate_matrix_path: Optional[str],
+    num_rate_categories: int,
+    random_seed: int,
+    optimize_rates: bool,
+) -> float:
+    """
+    This wrapper on top of run_phyml_cached that diverts logic to
+    get the metrics reported by the LG paper.
+    """
+    if model is not None and model.startswith("r__"):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        model_name = model.split('__')[-1]
+        df = pd.read_csv(os.path.join(dir_path, "Treebase.txt"), sep="\t")
+        df = df.drop(0)
+        df.set_index(["Name"], inplace=True)
+        protein_family_name = input_msa_path.split('/')[-1].split('.')[0]
+        return df.loc[protein_family_name, model_name]
+    else:
+        phyml_stats, _ = run_phyml_cached(
+            rate_matrix_path=rate_matrix_path,
+            model=model,
+            input_msa_path=input_msa_path,
+            num_rate_categories=num_rate_categories,
+            random_seed=random_seed,
+            optimize_rates=optimize_rates,
+        )
+        return get_phyml_ll_from_phyml_stats(phyml_stats)
+
+
+def get_filenames_Treebase():
+    # We sort families by number of taxa to ease testing with max_families
+    filenames = ['M2344', 'M1882', 'M1379', 'M1381', 'M1380', 'M1382', 'M964', 'M1046', 'M1496', 'M1384', 'M1385', 'M1383', 'M1497', 'M1502', 'M1498', 'M1506', 'M1374', 'M1507', 'M1989', 'M658', 'M2640', 'M2641', 'M2638', 'M1378', 'M2639', 'M2637', 'M1377', 'M1023', 'M2636', 'M1508', 'M1499', 'M1373', 'M1372', 'M1503', 'M2302', 'M1500', 'M2558', 'M1993', 'M1335', 'M2478', 'M2479', 'M2476', 'M1768', 'M2304', 'M1812', 'M1990', 'M1291', 'M1601', 'M1376', 'M1504', 'M2477', 'M2480', 'M2577', 'M337', 'M1487', 'M931', 'M730', 'M1392', 'M686']
+    # Put the two families with massive number of sites at the end.
+    filenames = [x for x in filenames if x not in ["M2577", "M964"]] + ["M2577", "M964"]
+    filenames = [x + '.clean.ProtGB_phyml' for x in filenames]
+    # filenames = sorted(list(os.listdir(treebase_dir)))
+    return filenames
+
+
 def reproduce_Treebase_JTT_WAG_LG(
     treebase_dir: str,
     num_rate_categories: int = 4,
@@ -216,13 +272,13 @@ def reproduce_Treebase_JTT_WAG_LG(
     max_families: int = 100000000,
     verbose: bool = False,
 ) -> pd.DataFrame:
-    filenames = sorted(list(os.listdir(treebase_dir)))
     rows = []
+    filenames = get_filenames_Treebase()
     for filename in filenames[:max_families]:
         protein_family_name = filename.split(".")[0]
         input_msa_path = os.path.join(treebase_dir, filename)
         row = {}
-        for model in ["JTT", "WAG", "LG"]:
+        for (model_name, model, rate_matrix_path) in MODELS:
             if verbose:
                 print(f"Processing: {filename} with {model}")
             row[model_name] = get_phyml_ll(
@@ -233,13 +289,13 @@ def reproduce_Treebase_JTT_WAG_LG(
                 random_seed=random_seed,
                 optimize_rates=optimize_rates,
             )
-            row[model] = get_phyml_ll(phyml_stats)
             row["Name"] = protein_family_name
-            row["Tax"] = get_number_of_taxa(phyml_stats)
-            row["Sites"] = get_number_of_sites(phyml_site_ll)
         rows.append(row)
     res = pd.DataFrame(
-        rows, columns=["Name", "Tax", "Sites", "JTT", "WAG", "LG"]
+        rows,
+        columns=[
+            "Name",
+        ] + [model[0] for model in MODELS]
     )
     res.sort_values(by=["Name"], inplace=True)
     res.reset_index(inplace=True)
@@ -250,18 +306,19 @@ def _run_phyml_cached(args) -> Tuple[float, int, int]:
     """
     Multiprocessing wrapper to call run_phyml_cached
     """
-    phyml_stats, phyml_site_ll = run_phyml_cached(
-        input_msa_path=args[0],
-        model=args[1],
-        rate_matrix_path=args[2],
-        num_rate_categories=args[3],
-        random_seed=args[4],
-        optimize_rates=args[5],
-    )
-    return (
-        get_phyml_ll(phyml_stats),
-        get_number_of_taxa(phyml_stats),
-        get_number_of_sites(phyml_site_ll),
+    input_msa_path = args[0]
+    model = args[1]
+    rate_matrix_path = args[2]
+    num_rate_categories = args[3]
+    random_seed = args[4]
+    optimize_rates = args[5]
+    return get_phyml_ll(
+        rate_matrix_path=rate_matrix_path,
+        model=model,
+        input_msa_path=input_msa_path,
+        num_rate_categories=num_rate_categories,
+        random_seed=random_seed,
+        optimize_rates=optimize_rates,
     )
 
 
@@ -280,11 +337,10 @@ def reproduce_Treebase_JTT_WAG_LG_parallel(
     """
     # First warm up the cache in parallel
     map_args = []
-    filenames = sorted(list(os.listdir(treebase_dir)))
+    filenames = get_filenames_Treebase()
     for filename in filenames[:max_families]:
         input_msa_path = os.path.join(treebase_dir, filename)
-        for model in ["JTT", "WAG", "LG"]:
-            rate_matrix_path = None
+        for (_, model, rate_matrix_path) in MODELS:
             map_args.append(
                 (
                     input_msa_path,
@@ -296,6 +352,7 @@ def reproduce_Treebase_JTT_WAG_LG_parallel(
                 )
             )
     map_func = _run_phyml_cached
+    np.random.shuffle(map_args)
     if n_process > 1:
         with multiprocessing.Pool(n_process) as pool:
             list(tqdm.tqdm(pool.imap(map_func, map_args), total=len(map_args)))
